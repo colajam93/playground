ff5b28a033f2e14d5e825822c22fc7fb3d1e6aaf
colajam93 <fs.fake.g1@gmail.com>
2016-01-27 02:09:54 +0900

    WIP add keyword argument to format

--- pptool/src/FormatPrint.hpp
+++ pptool/src/FormatPrint.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <iostream>
+#include <cstdio>
 #include <sstream>
+#include "KeywordArgument.hpp"
 
 namespace THI {
 
@@ -21,19 +22,33 @@ decltype(auto) formatImpl(std::stringstream& ss, const T& t,
     return formatImpl(ss, args...);
 }
 
-template <typename T, typename... Args>
-auto format(const T& t, const Args&... args)
+template <typename... Args>
+auto format(const Args&... args)
 {
     std::stringstream ss;
     ss << std::boolalpha;
-    return formatImpl(ss, t, args...).str();
+    return formatImpl(ss, args...).str();
 }
 
-template <typename T, typename... Args>
-void print(const T& t, const Args&... args)
+template <typename... Args>
+void print(const Args&... args)
 {
-    auto s = format(t, args...);
-    std::cout << s << std::endl;
+    ARGUMENT_PARSER
+    {
+        ADD_ARGUMENT_CONSTRUCTER(sep, " ");
+        ADD_ARGUMENT_CONSTRUCTER(end, "\n");
+    };
+    ADD_ARGUMENT(sep, " ");
+    ADD_ARGUMENT(end, "\n");
+    ARGUMENT_PARSER_END;
+
+    ArgumentParser parser;
+    auto values = THI::parseArgs(parser, args...);
+
+    // TODO
+    // change format use std::tuple
+    // auto s = format(values);
+    // std::fprintf(stdout, "%s%s", s.c_str(), parser.end);
 }
 
 } // namespace THI
