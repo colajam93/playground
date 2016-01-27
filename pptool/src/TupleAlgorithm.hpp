c3e14728493f22c4f38854e845458b3eff1387a2
colajam93 <fs.fake.g1@gmail.com>
2016-01-28 02:30:02 +0900

    add index argument to tupleForEach

--- pptool/src/TupleAlgorithm.hpp
+++ pptool/src/TupleAlgorithm.hpp
@@ -15,7 +15,7 @@ template <std::size_t N = 0, typename Func, typename... Tp,
           typename std::enable_if_t<(N < sizeof...(Tp))>* = nullptr>
 void tupleForEach(const Func& func, const std::tuple<Tp...>& tuple)
 {
-    func(std::get<N>(tuple));
+    func(N, std::get<N>(tuple));
     tupleForEach<N + 1, Func, Tp...>(func, tuple);
 }
 
