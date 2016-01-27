8b36e64b28bd8e6cd18d85e677b68238d87c7072
colajam93 <fs.fake.g1@gmail.com>
2016-01-28 01:44:38 +0900

    remove unused overload

--- pptool/src/TupleAlgorithm.hpp
+++ pptool/src/TupleAlgorithm.hpp
@@ -19,26 +19,4 @@ void tupleForEach(const Func& func, const std::tuple<Tp...>& tuple)
     tupleForEach<N + 1, Func, Tp...>(func, tuple);
 }
 
-template <std::size_t N = 0, typename Func, typename MergeFunc,
-          typename Identity, typename... Tp,
-          typename std::enable_if_t<N == sizeof...(Tp)>* = nullptr>
-decltype(auto) tupleForEach(const Func&, const MergeFunc&,
-                            const Identity& identity, const std::tuple<Tp...>&)
-{
-    return identity;
-}
-
-template <std::size_t N = 0, typename Func, typename MergeFunc,
-          typename Identity, typename... Tp,
-          typename std::enable_if_t<(N < sizeof...(Tp))>* = nullptr>
-decltype(auto) tupleForEach(const Func& func, const MergeFunc& mergeFunc,
-                            const Identity& identity,
-                            const std::tuple<Tp...>& tuple)
-{
-    auto&& result = func(std::get<N>(tuple));
-    return mergeFunc(result,
-                     tupleForEach<N + 1, Func, MergeFunc, Identity, Tp...>(
-                         func, mergeFunc, identity, tuple));
-}
-
 } // namespace THI
